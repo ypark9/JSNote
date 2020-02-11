b58bdf4d32b44d7f837d57404ce4d57bca6851a7
Park <ypark9@ncsu.edu>
2020-02-11 18:07:03 -0500

    IIFE?

--- B3-Global-Environment/B3_GlobalEnvironment/Starter/appS05.js
+++ B3-Global-Environment/B3_GlobalEnvironment/Starter/appS05.js
@@ -1,1 +1,20 @@
+// All the code is wrapped in the IIFE
+ (dothings = function () {
+    var firstName = "Yoon";
+    function init () {
+        doStuff (firstName);
+        // code to start the application
+    }
+    function doStuff () {
+        // Do stuff here
+        console.log("d0 stuff is here.");
+        
+    }
+    function doMoreStuff () {
+        // Do more stuff here
+    }
+    // Start the application
+    init ();
+}) ();
 
+dothings();
