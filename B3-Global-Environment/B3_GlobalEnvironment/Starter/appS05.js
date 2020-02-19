79ec7ad72015c8b65b15d2be9f955b8708bca99e
Park <ypark9@ncsu.edu>
2020-02-19 15:52:54 -0500

    practicing algorithm

--- B3-Global-Environment/B3_GlobalEnvironment/Starter/appS05.js
+++ B3-Global-Environment/B3_GlobalEnvironment/Starter/appS05.js
@@ -17,4 +17,42 @@
     init ();
 }) ();
 
+function palindrom(str){
+    let revStr = (str.split('')).reverse().join('');
+    var isPalin = false;
+
+    if(revStr === str)
+    isPalin = true;
+    
+    return isPalin;
+}
+
+function reverseInt(n) {
+    var isNeg = false;
+    if(parseInt(n) < 0){
+        isNeg = true;
+    }
+    let num = parseInt(n.toString().split('').reverse().join(''));
+    return isNeg? -1 * num : num;
+}
+
+function fib(n) {
+    if(n === 0)
+        return 0;
+    if(n === 1 || n === 2)
+        return 1;
+    else
+        return fib(n-2) + fib(n -1);
+}
+
+function anagrams(stringA, stringB) {
+    return stringA.split('').reverse().join('') === stringB;
+}
+
+console.log(anagrams('abba', 'baab'));
+
+console.log(fib(3));
+console.log(reverseInt(-500));
+console.log("palin " + palindrom('agggddgga'));
+
 dothings();
